390562a1bacb3fc0eadfd7fc044b24724ec85b07
naveenkumarchaprana-commits <naveenkumarchaprana@gmail.com>
2026-08-10 16:00:26 +0530

    Create set_intersection.py

---
+++ set_intersection.py
@@ -0,0 +1,12 @@
+# Problem: Find intersection of two sets
+
+# Approach:
+# Use built-in set intersection method
+
+# Time Complexity: O(n)
+
+e = {2, 3, 5, 6, 2}
+e2 = {2, 3, 4}
+
+result = e.intersection(e2)
+print("Intersection:", result)
